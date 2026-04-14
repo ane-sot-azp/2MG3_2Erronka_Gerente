b8f2aa8c2161fe442ad84284d1bdf57984da5966
iguisado228 <ira_gui_lor@goierrieskola.org>
2026-04-14 14:52:12 +0200

    Akatsak zuzenduta

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,32 +1,33 @@
 module org.example._erronka_java {
+    requires javafx.base;
+    requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires java.sql;
-    requires javafx.graphics;
-    requires java.net.http;
-    requires com.google.gson;
-    requires javafx.base;
     requires java.desktop;
+    requires java.net.http;
+    requires java.sql;
+
+    requires com.google.gson;
+    requires net.synedra.validatorfx;
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
     requires org.apache.pdfbox;
-    requires unirest.java;
     requires tools.jackson.databind;
+    requires unirest.java;
 
     opens Pantailak to javafx.fxml;
     opens Klaseak to javafx.base, com.google.gson;
-    opens icons;
     opens css;
     opens services to com.google.gson;
+    opens icons;
 
     exports Klaseak;
     exports Pantailak;
     exports services;
+    exports DB;
 
 }
